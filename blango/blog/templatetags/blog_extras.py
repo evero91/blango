3aacd692f5ee3db9ada6850d1fe8205a8759d8b1
Everardo Guevara <eguevara@sistemaspremium.com>
2024-05-23 16:53:02 -0600

    simple_tag

--- blango/blog/templatetags/blog_extras.py
+++ blango/blog/templatetags/blog_extras.py
@@ -7,9 +7,19 @@ register = template.Library()
 user_model = get_user_model()
 
 @register.filter(name='author_details')
-def author_details(author):
+def author_details(author, current_user):
+# @register.simple_tag(takes_context=True)
+# def author_details_tag(context):
+    # request = context['request']
+    # current_user = request.user
+    # post = context['post']
+    # author = post.author
+
     if not isinstance(author, user_model):
         return ''
+    
+    if author == current_user:
+        return format_html('<strong>me</strong>')
     
     prefix = ''
     suffix = ''
@@ -26,3 +36,19 @@ def author_details(author):
 
     # return mark_safe(f'{prefix}{escape(name)}{suffix}')
     return format_html('{}{}{}', prefix, name, suffix)
+
+@register.simple_tag
+def row(extra_classes=''):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html('</div>')
+
+@register.simple_tag
+def col(extra_classes=''):
+    return format_html('<div class="col {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+    return format_html('</div>')
